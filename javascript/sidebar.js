a5bc0a9e66bed4e7e6adf5d9b21c9e5d2694f61b
Lucas Lyra <lucalyra@gmail.com>
2019-01-29 13:11:16 +0200

    Added logo and a close button

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,11 +1,13 @@
 class MoreOptions{
     constructor(){
         this.moreOptions = document.querySelector(".header-mobile-more");
+        this.sidebarMore = document.querySelector(".sidebar-more-button");
         this.moreButton = document.querySelector(".more-button")
         this.moreBox = document.querySelector(".sidebar-menu");
         this.moreBackground = document.querySelector(".sidebar-screen-background")
         this.openMore()
         this.backgroundClose()
+        this.sidebarMoreButton()
     }
 
     openMore(){
@@ -19,6 +21,13 @@ class MoreOptions{
     }
     backgroundClose(){
         this.moreBackground.addEventListener('click', () =>{ 
+            if(this.moreBox.classList.contains("hide") == false){
+                this.moreBox.classList.add("hide")
+            }
+        } )
+    }
+    sidebarMoreButton(){
+        this.sidebarMore.addEventListener('click', () =>{ 
             if(this.moreBox.classList.contains("hide") == false){
                 this.moreBox.classList.add("hide")
             }

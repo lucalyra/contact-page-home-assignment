0ef781dbfce25145430eaf47d74ab02cda3be7a0
Lucas Lyra <lucalyra@gmail.com>
2019-01-29 20:09:53 +0200

    Added menu description

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -5,31 +5,35 @@ class MoreOptions{
         this.moreButton = document.querySelector(".more-button")
         this.moreBox = document.querySelector(".sidebar-menu");
         this.moreBackground = document.querySelector(".sidebar-screen-background")
+        this.body = document.querySelector("body")
         this.openMore()
         this.backgroundClose()
         this.sidebarMoreButton()
     }
 
     openMore(){
-        this.moreButton.addEventListener("click",() => { if( this.moreBox.classList.contains("hide") ) {
-            this.moreBox.classList.remove("hide")
-        } else { this.closeMore()  }
+        this.moreButton.addEventListener("click",() => { 
+            if( this.moreBox.classList.contains("hide") ) {
+                this.moreBox.classList.remove("hide")
+                this.body.style.overflow = "hidden"
+            } else { this.closeMore() }
          } )
     }
     closeMore(){
         this.moreBox.classList.add("hide")
+        this.body.style.overflow = "auto"
     }
     backgroundClose(){
         this.moreBackground.addEventListener('click', () =>{ 
             if(this.moreBox.classList.contains("hide") == false){
-                this.moreBox.classList.add("hide")
+                this.closeMore()
             }
         } )
     }
     sidebarMoreButton(){
         this.sidebarMore.addEventListener('click', () =>{ 
             if(this.moreBox.classList.contains("hide") == false){
-                this.moreBox.classList.add("hide")
+                this.closeMore()
             }
         } )
     }
